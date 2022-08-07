a9669a783744ad0af7acddd3299db8dc6aebd77d
cevahir72 <kelesmevlut71@gmail.com>
2022-08-08 00:45:36 +0300

    checkout-v1

--- my-app/src/pages/Checkout.js
+++ my-app/src/pages/Checkout.js
@@ -1,12 +1,74 @@
-import React from 'react'
+import React from "react";
+import { useSelector , useDispatch} from "react-redux";
+import { Link } from "react-router-dom";
+import {resetCart} from "../redux/cartSlice";
+import "../style/Checkout.css"
 
 const Checkout = () => {
+
+  const cartItem = useSelector((state) => state.cart.cartItems);
+
+  const dispatch = useDispatch()
+
   return (
-    <div>
-
-        <h1>Checkout Page</h1>
+    <div className="checkout">
+      <h1>Shopping Cart</h1>
+      <div className="checkout-inner">
+          <div className="chechout-inner-title">
+            <div className="checkout-inner-title-A">
+              <h2>Title</h2>
+            </div>
+            <div className="checkout-inner-title-B">
+              <h2>Price</h2>
+            </div>
+            <div className="checkout-inner-title-B">
+              <h2>Quantity</h2>
+            </div>
+            <div className="checkout-inner-title-B">
+              <h2>Total</h2>
+            </div>
+          </div>
+          {cartItem.map((item) => {
+            return(
+              <div className="cart-items-" key={item.id}>
+              <div className="cart-items-A">
+                <img src={item.image} alt={item.title} />
+                <span>{item.title}</span>
+              </div>
+              <div className="cart-items-B">
+                <h2>${item.price}</h2>
+              </div>
+              <div className="cart-items-B">
+                <h2>{item.cartQuantity}</h2>
+                <span>Remove</span>
+              </div>
+              <div className="cart-items-B">
+                <h2>${item.cartQuantity * item.price}</h2>
+              </div>
+            </div>
+            )
+            
+          })}
+          <div className="cart-summary">
+            <button onClick={()=>dispatch(resetCart())} className="clear-cart">Clear Cart</button>
+            <div className="cart-checkout">
+              <div className="subtotal">
+                <span>Subtotal</span>
+                <span>$TOTAL</span>
+              </div>
+              <p>Taxes and Shipping Calculated at checkout</p>
+              <button>Check out</button>
+              <div className="continiue-shopping">
+                <Link to="/">
+                  <ion-icon name="arrow-back-outline"></ion-icon>
+                  <span>Continiue Shopping</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;
--- my-app/src/redux/cartSlice.js
+++ my-app/src/redux/cartSlice.js
@@ -3,7 +3,7 @@ import {toast} from "react-toastify";
 
 
 const initialState = {
-    cartItems: [],
+    cartItems: localStorage.getItem("cartItems" ) ? JSON.parse(localStorage.getItem("cartItems")): [],
     cartTotalQuantity: 0,
     cartTotalAmount: 0
 }
@@ -18,16 +18,17 @@ export const cartSlice = createSlice({
         const itemIndex = state.cartItems.findIndex(item=> item.id === action.payload.id);
         if(itemIndex >= 0){ 
             state.cartItems[itemIndex].cartQuantity +=1
-            toast.info("increased product quantity", {
+            toast.info(`increased ${state.cartItems[itemIndex].name} quantity`, {
                 position:"bottom-left"
             });
         }else{
             const tempProducts = {...action.payload, cartQuantity:1}
             state.cartItems.push(tempProducts);
-            toast.success("added a new product to cart", {
+            toast.success(`added ${action.payload.name} to cart`, {
                 position:"bottom-left"
             });
         }
+        localStorage.setItem("cartItems",JSON.stringify(state.cartItems));
         
        },
        getTotals(state,action){
@@ -50,10 +51,17 @@ export const cartSlice = createSlice({
         state.cartTotalQuantity = quantity;
         state.cartTotalAmount = total;
 
+       },
+
+       resetCart(state, action) {
+        state.cartItems = [];
+        localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+        toast.error("Cart cleared", { position: "bottom-left" })
        }
+
     }
 })
 
-export const { addToCart, getTotals} = cartSlice.actions
+export const { addToCart, getTotals, resetCart} = cartSlice.actions
 
 export default cartSlice.reducer
